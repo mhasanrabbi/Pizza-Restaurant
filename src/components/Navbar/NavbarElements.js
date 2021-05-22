ef19df456740b68d88be508646038da2c8bb3c10
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-05-22 20:39:54 +0600

    insert react-icons

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
-import { NavLink as Link} from 'react-router-dom'
-
+import { NavLink as Link} from 'react-router-dom';
+import { FaPizzaSlice} from 'react-icons/fa';
 export const Nav = styled.nav`
-  background: transparent;
+  background: red;
   height: 80px;
   display: flex;
   justify-content: center;
@@ -17,9 +17,28 @@ export const NavLink = styled(Link)`
   text-decoration: none;
   cursor: pointer;
 
-  @media screen and (max-width: 400px) {
-    position: absolute;
-    top: 10px;
-    left: 25px;
+    @media screen and (max-width: 400px) {
+      position: absolute;
+      top: 10px;
+      left: 25px;
+    }
+`
+
+export const NavIcon = styled.div`
+  display: block;
+  position: absolute;
+  top: 0;
+  right: 0;
+  cursor: pointer;
+  color: #fff;
+
+  p {
+    transform: translate(-175%, 100%);
+    font-weight: bold;
   }
+`
+
+export const Bars = styled(FaPizzaSlice)`
+  font-size: 2rem;
+  transform: translate(-50%, -15%);
 `
