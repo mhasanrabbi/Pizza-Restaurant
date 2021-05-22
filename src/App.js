b98eed382a40b683d977b3590b578327e2223380
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-05-22 20:53:12 +0600

    apply global styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
 
 function App() {
   return (
     <Router>
+    <GlobalStyle />
      <Navbar />
     </Router>
   );
