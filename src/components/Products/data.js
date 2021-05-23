94bd1659e57d991c426adbac67819b53e56b4a3c
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-05-23 15:13:35 +0600

    insert dessert component

--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -1,6 +1,9 @@
-import product1 from '../../assets/images/product-1.jpg';
-import product2 from '../../assets/images/product-2.jpg';
-import product3 from '../../assets/images/product-3.jpg';
+import product1 from '../../images/product-1.jpg';
+import product2 from '../../images/product-2.jpg';
+import product3 from '../../images/product-3.jpg';
+import sweet1 from '../../images/sweet-2.jpg';
+import sweet2 from '../../images/sweet-3.jpg';
+import sweet3 from '../../images/sweet4.jpg';
 
 export const productData = [
   {
@@ -27,4 +30,31 @@ export const productData = [
     price: '$13.99',
     button: 'Add to Cart'
   }
-]
+];
+
+export const productDataTwo = [
+  {
+    img: sweet1,
+    alt: 'Donuts',
+    name: 'Doughlicious',
+    desc: 'Belgian chocolate glazed donuts covered in icing with sprinkles on top',
+    price: '$9.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet2,
+    alt: 'Ice Cream',
+    name: 'Caramel Wonder',
+    desc: 'Vanilla ice cream covered with caramel and chocolate glaze topped with a coco stick',
+    price: '$12.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet3,
+    alt: 'Brownie',
+    name: 'Brownie Bunch',
+    desc: 'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
+    price: '$9.99',
+    button: 'Add to Cart'
+  }
+];
