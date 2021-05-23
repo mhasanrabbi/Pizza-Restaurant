c19deff98ddc55f121980361b047cfa7b2b8abb8
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-05-23 14:36:44 +0600

    product component  insert

---
+++ src/components/Products/data.js
@@ -0,0 +1,30 @@
+import product1 from '../../assets/images/product-1.jpg';
+import product2 from '../../assets/images/product-2.jpg';
+import product3 from '../../assets/images/product-3.jpg';
+
+export const productData = [
+  {
+    img: product1,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc: 'Marinara sauce, basil, italian sasuage, roma tomatoes, olives and pesto',
+    price: '$19.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: product2,
+    alt: 'Pizza',
+    name: 'Hawaian Paradise',
+    desc: 'Marinara sauce, basil, italian sasuage, roma tomatoes, olives and pesto',
+    price: '$16.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: product3,
+    alt: 'Pizza',
+    name: 'Veggie Overload',
+    desc: 'Marinara sauce, basil, italian sasuage, roma tomatoes, olives and pesto',
+    price: '$13.99',
+    button: 'Add to Cart'
+  }
+]
